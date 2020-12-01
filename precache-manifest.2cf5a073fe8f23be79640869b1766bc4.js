13dc3a4dd8901c66d098ce9265610fea7a76c91f
Manoj Mukherjee <manojmukherjee777@gmail.com>
2020-12-01 12:13:44 +0530

    Updates

---
+++ precache-manifest.2cf5a073fe8f23be79640869b1766bc4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd8d4ce61ae4e9b481df1b7bea61793",
+    "url": "/Sathi_Invite_U/index.html"
+  },
+  {
+    "revision": "2157fafbd9ae116f27fc",
+    "url": "/Sathi_Invite_U/static/css/main.577fb959.chunk.css"
+  },
+  {
+    "revision": "a32b37c549ddf60dda83",
+    "url": "/Sathi_Invite_U/static/js/2.bf589217.chunk.js"
+  },
+  {
+    "revision": "2157fafbd9ae116f27fc",
+    "url": "/Sathi_Invite_U/static/js/main.91775504.chunk.js"
+  },
+  {
+    "revision": "fd55721966273d890999",
+    "url": "/Sathi_Invite_U/static/js/runtime~main.d702e400.js"
+  },
+  {
+    "revision": "4fecfaadb34f86867d0c7ae5c25e315f",
+    "url": "/Sathi_Invite_U/static/media/Background_Circle.4fecfaad.png"
+  },
+  {
+    "revision": "9b14f9e6765b6de220768a0df0fc9109",
+    "url": "/Sathi_Invite_U/static/media/a123.9b14f9e6.png"
+  },
+  {
+    "revision": "7f5bc7f3913b72e421983e869024afed",
+    "url": "/Sathi_Invite_U/static/media/ap.7f5bc7f3.png"
+  },
+  {
+    "revision": "155af7fd1e56340dc2cec7bd2761e203",
+    "url": "/Sathi_Invite_U/static/media/brideback.155af7fd.jpg"
+  },
+  {
+    "revision": "0848d9d0f415ea63d3ecce98e586b461",
+    "url": "/Sathi_Invite_U/static/media/counter-1-1600x600.0848d9d0.jpg"
+  },
+  {
+    "revision": "82c8fbe284a653442e684c21bbe4336c",
+    "url": "/Sathi_Invite_U/static/media/logo-white.82c8fbe2.png"
+  },
+  {
+    "revision": "e70220b1eaa62271881c408ee0c2c843",
+    "url": "/Sathi_Invite_U/static/media/rajeshm.e70220b1.jpeg"
+  },
+  {
+    "revision": "bf69246006360e41647ecae601e247fa",
+    "url": "/Sathi_Invite_U/static/media/slider-1-1600x900.bf692460.jpg"
+  },
+  {
+    "revision": "e91a4271617be28141818be957997f84",
+    "url": "/Sathi_Invite_U/static/media/somam.e91a4271.jpeg"
+  },
+  {
+    "revision": "32d27b8e2d1309228e888d54285197d3",
+    "url": "/Sathi_Invite_U/static/media/w.32d27b8e.png"
+  },
+  {
+    "revision": "19ae42dbe0f8ac04f58993f4e70425a6",
+    "url": "/Sathi_Invite_U/static/media/w22.19ae42db.png"
+  },
+  {
+    "revision": "1f0312b65ec66e7a3ade40aecf48a6af",
+    "url": "/Sathi_Invite_U/static/media/webzed-font-1.1f0312b6.woff"
+  },
+  {
+    "revision": "6ed2107f2da89dcf9e52f006e878b679",
+    "url": "/Sathi_Invite_U/static/media/webzed-font-1.6ed2107f.ttf"
+  },
+  {
+    "revision": "d5939ecfb23c9b4504264e0c4234ba30",
+    "url": "/Sathi_Invite_U/static/media/webzed-font-1.d5939ecf.eot"
+  },
+  {
+    "revision": "e65681d7a99427774c50261520216889",
+    "url": "/Sathi_Invite_U/static/media/webzed-font-1.e65681d7.svg"
+  }
+]);
